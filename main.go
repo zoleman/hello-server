d171800f36682e8ea2a7bb529f16d18fe8ead949
Zoltan Imre <pugomagitor@gmail.com>
2026-09-01 21:07:24 +0200

    Initial Go HTTP server

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server listening on http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
