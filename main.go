23a1511bb72b59083cf4c966b7221de25c52f56b
Zoltan Imre <pugomagitor@gmail.com>
2026-09-02 14:55:21 +0200

    Add graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,17 +15,52 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "healthy")
+	fmt.Fprintln(w, "healthy")
 }
 
 func main() {
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/", helloHandler)
 
+	server := &http.Server{
+		Addr: ":8080",
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		fmt.Println("Server listening on http://localhost:8080")
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Server error: %v\n", err)
+		}
+
+	case sig := <-shutdown:
+		fmt.Printf("Received %s, shutting down gracefully...\n", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			fmt.Printf("Graceful shutdown failed: %v\n", err)
+		}
+
+		fmt.Println("Server stopped")
+	}
+
+	/*old server
 	fmt.Println("Server listening on http://localhost:8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+	*/
 }
